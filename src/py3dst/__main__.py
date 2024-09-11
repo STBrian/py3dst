2571d6502d6935ea972cf682e100ee6fdc1e0d53
STBrian <brichap100@gmail.com>
2024-09-10 18:33:02 -0600

    Changes in main

--- src/py3dst/__main__.py
+++ src/py3dst/__main__.py
@@ -1,5 +1,8 @@
-import argparse, sys, os
+import argparse
+import sys
+import os
 import tkinter
+import traceback
 from PIL import Image, ImageTk, UnidentifiedImageError
 from glob import iglob
 from pathlib import Path
@@ -7,6 +10,49 @@ from .tex3dst import Texture3dst
 from .error_classes import *
 
 __version__ = "1.1.1"
+
+def convertFile(input_path: Path, output_path: Path, show_unidentified_image: bool, show_tracebacks: bool):
+    try:
+        texture = Texture3dst().open(input_path)
+        try:
+            image = texture.copy(0, 0, texture.size[0], texture.size[1])
+            if not output_path.exists():
+                os.makedirs(output_path)
+            image.save(f"{output_path}/{input_path.stem}.png")
+            print("File saved at:", f"{output_path.absolute()}/{input_path.stem}.png")
+        except Exception as e:
+            print("Error: Unable to convert file:", e)
+            print(input_path.absolute())
+            if show_tracebacks:
+                traceback.print_exc()
+            return 6
+    except Texture3dstNoSignature:
+        try:
+            image = Image.open(input_path)
+            try:
+                texture = Texture3dst().fromImage(image)
+                if not output_path.exists():
+                    os.makedirs(output_path)
+                texture.export(f"{output_path}/{input_path.stem}.3dst")
+                print("File saved at:", f"{output_path.absolute()}/{input_path.stem}.3dst")
+            except Exception as e:
+                print("Error: Unable to convert file:", e)
+                print(input_path.absolute())
+                if show_tracebacks:
+                    traceback.print_exc()
+                return 8
+        except UnidentifiedImageError:
+            if show_unidentified_image:
+                print("Error: Unable to convert file")
+                print(input_path.absolute())
+            return 7
+    except Exception as e:
+        print("Error: Unable to convert file:", e)
+        print(input_path.absolute())
+        if show_tracebacks:
+            traceback.print_exc()
+        return 5
+    return 0
 
 def main():
     parser = argparse.ArgumentParser(prog="py3dst", description="Display or convert 3DST textures")
@@ -16,10 +62,40 @@ def main():
         help="path to file to open and show"
     )
     parser.add_argument(
+        "-t", 
+        "--touch", 
+        action="store_true",
+        help="textures provided will be rebuilded"
+    )
+    parser.add_argument(
+        "--suppress-errors", 
+        action="store_true",
+        help="the program will continue even if it finds a non-critical error in a file"
+    )
+    parser.add_argument(
+        "--show-tracebacks", 
+        action="store_true",
+        help="this will show tracebacks when a file is not converted fro unhandled reasons"
+    )
+    parser.add_argument(
         "-c", 
         "--convert", 
         action="store_true",
         help="indicates whether to convert the provided file"
+    )
+    parser.add_argument(
+        "-i", 
+        "--input", 
+        metavar=("IN"),
+        action="append",
+        help="files to convert"
+    )
+    parser.add_argument(
+        "-o", 
+        "--output", 
+        metavar=("OUT"),
+        action="store",
+        help="destination directory"
     )
     parser.add_argument(
         "-r", 
@@ -34,127 +110,30 @@ def main():
         metavar=("FORMAT"),
         choices=["rgba8", "rgb8", "rgba5551", "rgb565", "rgba4", "la8", "la4"], 
         default="rgba8",
-        help="(optional) color format for the output ('rgba8', 'rgb8', 'rgba5551', 'rgb565', 'rgba4', 'la8', 'la4')"
-    )
-    parser.add_argument(
-        "-o", 
-        "--output", 
-        metavar=("OUT"),
-        action="store",
-        help="destination file or directory if multiple output files"
+        help="color format for the output ('rgba8', 'rgb8', 'rgba5551', 'rgb565', 'rgba4', 'la8', 'la4')"
     )
     parser.add_argument("-v", "--version", action="version", version=__version__)
 
     args = parser.parse_args()
+    if args.touch and args.convert:
+        parser.error("conflicting flags, select only one -t --touch or -c --convert")
+    if args.touch and not args.path:
+        parser.error("path is required with -t --touch flag")
     if not args.convert and not args.path:
         parser.error("path is required if not -c --convert flag used")
+    if args.convert and not args.input:
+        parser.error("-i --input is required if -c --convert flag used")
     if args.convert and not args.output:
         parser.error("-o --output is required if -c --convert flag used")
 
-    path = Path(args.path)
-    if args.convert:
-        output_path = Path(args.output)
-        if path.is_file():
-            try:
-                texture = Texture3dst().open(path)
-                try:
-                    image = texture.copy(0, 0, texture.size[0], texture.size[1])
-                    image.save(output_path)
-                    print("File saved at:", output_path.absolute())
-                except Exception as e:
-                    print("Error: Unable to convert file:", e)
-                    sys.exit(7)
-            except Texture3dstNoSignature:
-                try:
-                    image = Image.open(path)
-                    try:
-                        texture = Texture3dst().fromImage(image)
-                        texture.export(output_path)
-                    except Exception as e:
-                        print("Error: Unable to convert file:", e)
-                        sys.exit(8)
-                except Exception as e:
-                    print("Error: Unable to load file:", e)
-                    sys.exit(6)
-            except Exception as e:
-                print("Error: Unable to load file:", e)
-                sys.exit(5)
-        elif path.is_dir():
-            print("The path is a directory. Select an option:")
-            print("1) Convert 3DST textures to PNG images")
-            print("2) Convert PNG images to 3DST textures")
-            print("3) Convert all supported image formats to 3DST textures")
-            print("0) Exit")
-            option = input("Enter an option: ")
-            match option:
-                case "1":
-                    if args.recursive:
-                        input_files = iglob(os.path.join(path.absolute(), "**/*.3dst"), recursive=True)
-                    else:
-                        input_files = iglob(os.path.join(path.absolute(), "*.3dst"))
-
-                    for file in input_files:
-                        file_path = Path(file)
-                        try:
-                            texture = Texture3dst().open(file)
-                            image = texture.copy(0, 0, texture.size[0], texture.size[1])
-                            output_file_path = Path(os.path.join(output_path.absolute(), f"{file_path.stem}.png"))
-                            if not output_path.exists():
-                                os.makedirs(output_path)
-                            image.save(output_file_path)
-                            print("File saved at:", output_file_path.absolute())
-                        except:
-                            print("Unable to convert:", file_path.absolute())
-                case "2":
-                    if args.recursive:
-                        input_files = iglob(os.path.join(path, "**/*.png"), recursive=True)
-                    else:
-                        input_files = iglob(os.path.join(path, "*.png"))
-
-                    for file in input_files:
-                        file_path = Path(file)
-                        try:
-                            image = Image.open(file_path)
-                            texture = Texture3dst().fromImage(image)
-                            output_file_path = Path(os.path.join(output_path, f"{file_path.stem}.3dst"))
-                            if not output_path.exists():
-                                os.makedirs(output_path)
-                            texture.export(output_file_path)
-                            print("File saved at:", output_file_path.absolute())
-                        except:
-                            print("Unable to convert:", file_path.absolute())
-                case "3":
-                    input_files = iglob(os.path.join(path.absolute(), "**"), recursive=args.recursive)
-
-                    for file in input_files:
-                        file_path = Path(file)
-                        try:
-                            image = Image.open(file_path)
-                            texture = Texture3dst().fromImage(image)
-                            output_file_path = Path(os.path.join(output_path.absolute(), f"{file_path.stem}.3dst"))
-                            if not output_path.exists():
-                                os.makedirs(output_path)
-                            texture.export(output_file_path)
-                            print("File saved at:", output_file_path.absolute())
-                        except UnidentifiedImageError:
-                            pass
-                        except:
-                            print("Unable to convert:", file_path.absolute())
-                case "0":
-                    sys.exit(0)
-                case _:
-                    print("Error: Invalid option")
-            pass
-        else:
-            print("Error: Unable to open path")
-            sys.exit(4)
-    else:
+    if not args.convert and not args.touch:
+        path = Path(args.path)
         if path.exists() and path.is_file():
             try:
                 texture = Texture3dst().open(path)
             except Exception as e:
                 print("Error: Unable to load 3dst texture:", e)
-                sys.exit(3)
+                return 3
 
             image = texture.copy(0, 0, texture.size[0], texture.size[1])
             
@@ -168,10 +147,56 @@ def main():
             root.mainloop()
         elif path.exists() and path.is_dir():
             print("Error: Path is a directory")
-            sys.exit(2)
+            return 2
         else:
-            print("Error: The file doesn't exists")
-            sys.exit(1)
+            print("Error: Path doesn't exists")
+            return 1
+    elif args.touch:
+        path = Path(args.path)
+        if path.exists() and path.is_file():
+            try:
+                texture = Texture3dst().open(path)
+            except Exception as e:
+                print("Error: Unable to load 3dst texture:", e)
+                return 3
+            texture.export(path)
+        elif path.exists() and path.is_dir():
+            print("Error: Path is a directory")
+            return 2
+        else:
+            print("Error: Path doesn't exists")
+            return 1
+    elif args.convert:
+        output_path = Path(args.output)
+        
+        for path in args.input:
+            input_path = Path(path)
+            if input_path.exists() and input_path.is_file():
+                input_path = Path(path)
+                status_code = convertFile(input_path, output_path, show_unidentified_image=True, show_tracebacks=args.show_tracebacks)
+                if not args.suppress_errors and status_code:
+                    return status_code
+            elif input_path.exists() and input_path.is_dir():
+                if args.recursive:
+                    input_files = iglob(os.path.join(input_path.absolute(), "**/**"), recursive=True)
+                else:
+                    input_files = iglob(os.path.join(input_path.absolute(), "**"))
+
+                for file in input_files:
+                    file_path = Path(file)
+                    if file_path.is_file():
+                        status_code = convertFile(file_path, output_path, show_unidentified_image=False, show_tracebacks=args.show_tracebacks)
+                        if status_code and not args.suppress_errors and status_code != 7:
+                            return status_code
+            else:
+                print("Error: Path doesn't exists")
+                return 1
+    else:
+        print("Nothing has happened?")
+
+    return 0
+        
 
 if __name__ == "__main__":
-    main()
+    status_code = main()
+    sys.exit(status_code)
