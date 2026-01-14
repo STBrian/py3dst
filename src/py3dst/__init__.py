ea5a1357690b5f411671d065660e0fb2090cb695
STBrian <brichap100@gmail.com>
2026-01-13 19:17:48 -0600

    Bump version 1.2.2 -> 1.3.0

--- src/py3dst/__init__.py
+++ src/py3dst/__init__.py
@@ -2,7 +2,7 @@
 # This file is part of 'py3dst' and is licensed under the GPLv3.
 # See <https://www.gnu.org/licenses/> for details.
 
-__version__ = "1.2.2"
+__version__ = "1.3.0"
 
 from .tex3dst import Texture3dst
 from .error_classes import Texture3dstException, Texture3dstNoSignature, Texture3dstUnsupported
--- src/py3dst/__main__.py
+++ src/py3dst/__main__.py
@@ -13,7 +13,7 @@ from pathlib import Path
 from .tex3dst import Texture3dst
 from .error_classes import *
 
-__version__ = "1.2.2"
+__version__ = "1.3.0"
 
 def convertFile(input_path: Path, output_path: Path, show_unidentified_image: bool, show_tracebacks: bool):
     try:
